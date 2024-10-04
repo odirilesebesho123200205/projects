f734e1527ca55ee045dc6f24358bac26a16a4867
Herman Lintvelt <herman.lintvelt@gmail.com>
2024-10-04 13:48:21 +0200

    updating grade.sh for de week1

--- dataengineering/de-ex-week1/grade.sh
+++ dataengineering/de-ex-week1/grade.sh
@@ -1,20 +1,19 @@
 cp ./solution_output.csv $1/solution_output.csv
-cp ./orders.csv $1/orders.csv
-cp ./users.csv $1/users.csv
 cd $1
 
-if [ -e prepare.sh ]; then
-    echo "Detected prepare.sh script"
-    ./prepare.sh
-fi
-
-if [ ! -e wtc_week1.ipynb ]; then
-    echo "wtc_week1.ipynb was not found"
-    exit 1
-fi
-
-#Execute notebook
-jupyter execute wtc_week1.ipynb
+#if [ -e prepare.sh ]; then
+#    echo "Detected prepare.sh script"
+#    ./prepare.sh
+#fi
+#
+#if [ ! -e wtc_week1.ipynb ]; then
+#    echo "wtc_week1.ipynb was not found"
+#    exit 1
+#fi
+#
+##Execute notebook
+#echo "Executing Notebook wtc_week1.ipynb"
+#jupyter execute wtc_week1.ipynb
 
 #compare output CSV file
 file1="output.csv"
