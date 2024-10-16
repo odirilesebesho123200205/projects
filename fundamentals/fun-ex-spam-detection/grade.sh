44cfc22905ca7cee30ab9d8db4688d18b48eb05f
Fen Faure <fen@wethinkcode.co.za>
2024-10-16 15:52:24 +0200

    More robust grade.sh for Spam Detection project

--- fundamentals/fun-ex-spam-detection/grade.sh
+++ fundamentals/fun-ex-spam-detection/grade.sh
@@ -1,11 +1,11 @@
-if [ -e $1/prepare.sh ]; then
+if [ -e "$1/prepare.sh" ]; then
     echo "Detected prepare.sh script"
     ./prepare.sh
 fi
 
-if [ ! -e $1/spam.py ]; then
+if [ ! -e "$1/spam.py" ]; then
     echo "spam.py was not found"
     exit 1
 fi
 
-python3 ./accuracy.py $1/spam.py
+python3 ./accuracy.py "$1/spam.py"
