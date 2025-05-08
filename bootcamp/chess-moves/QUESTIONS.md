4e733c570a3df08b6a886f29db20679fffea95ad
Fen Faure <fen@wethinkcode.co.za>
2025-05-08 08:58:35 +0200

    Improve bootcamp project

---
+++ bootcamp/chess-moves/QUESTIONS.md
@@ -0,0 +1,12 @@
+- Does the parse_fen function return enough information to generate the legal moves for a given position?
+
+- Is it easy to use the board representation to find legal moves?
+
+- Can every member of the team explain a given FEN string and its components?
+
+- Does the generate_moves function elegantly allow the distinction between moves that can't, or can only, capture?
+
+- What was your approach to teamwork?
+
+- What improvements remain?
+
--- bootcamp/chess-moves/test_chess.py
+++ bootcamp/chess-moves/test_chess.py
@@ -8,3 +8,21 @@ def test_opening_moves():
 
     moves = chess.generate_moves(board)
     assert len(moves) == 20, f"Expected 20 moves, but got {len(moves)}"
+
+
+def test_zukertort_1():
+    board = chess.parse_fen(
+        "rnbqkbnr/pppppppp/8/8/8/5N2/PPPPPPPP/RNBQKB1R b KQkq - 1 1"
+    )
+
+    moves = chess.generate_moves(board)
+    assert len(moves) == 20, f"Expected 20 moves, but got {len(moves)}"
+
+
+def test_zukertort_2():
+    board = chess.parse_fen(
+        "rnbqkbnr/pppp1ppp/4p3/8/8/5N2/PPPPPPPP/RNBQKB1R w KQkq - 0 2"
+    )
+
+    moves = chess.generate_moves(board)
+    assert len(moves) == 23, f"Expected 23 moves, but got {len(moves)}"
