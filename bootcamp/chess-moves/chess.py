815c005a6a82d4dab88ba3243df0a4317f45e079
TshegofatsoMkhabela <tshego300@gmail.com>
2026-07-24 11:42:53 +0200

    team planning stage

--- bootcamp/chess-moves/chess.py
+++ bootcamp/chess-moves/chess.py
@@ -28,10 +28,15 @@ pieces = [
 , to_move, castling_rights, ep, hm, fm 
 """
 
+def main():
+    board = parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    display_board(board)
+    print(generate_moves(board))
+
 def parse_fen(fen):
-    fen_pieces= fen.split(" ")
+    fen_pieces, to_move, castling_rights, ep, hm, fm = fen.split(" ")
     pieces = [[]]
-    for char in fen:
+    for char in fen_pieces:
         if char.isdigit(): # we are checking if a character is a digit 
             pieces[-1].extend(["."] * int(char)) #the code is saying that if it is indeed a digit, it must 
         elif char == "/":
@@ -39,18 +44,47 @@ def parse_fen(fen):
         else:
             pieces[-1].append(char)
 
-    
-    for row in pieces:
-        print(" ".join(row))
-
     return pieces
 
-parse_fen("rnbqkbnr/pppp1ppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
-
-
 def generate_moves(board):
-    raise NotImplementedError("This function is not implemented yet.")
+    return (
+         knight_moves(board) +
+         king_moves(board) +
+         rook_moves(board) + 
+         bishop_moves(board) +
+         queen_moves(board)
+    )
+
+def knight_moves(board):
+    """Returns number of legal moves a knight can make"""
+    return -1
+
+def king_moves(board):
+    """Returns the number of legal moves a king can make"""
+    return -1
+
+def rook_moves(board):
+    """Returns the number of legal moves a rook can make"""
+    return -1
+
+def bishop_moves(board):
+    """Returns the number of legal moves a bishop can make"""
+    return -1
+
+def queen_moves(board):
+    """Returns the number of legal queen"""
+    return -1
 
 
 def apply_move(board, move):
     raise NotImplementedError("This function is not implemented yet.")
+
+def display_board(board):
+    """Prints the chess board"""
+    for row in board:
+            print(" ".join(row))
+
+def is_in_board(location, board):
+    """Determines whether a location is inside the board"""
+
+main()
